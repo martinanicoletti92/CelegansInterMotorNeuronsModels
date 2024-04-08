e1651095b19519c70e553da2a91de7a19fe3ff75
martinanicoletti92 <34269874+martinanicoletti92@users.noreply.github.com>
2024-04-08 18:10:23 +0200

    Update RIM_simulation_iclamp.py

--- RIM_simulation_iclamp.py
+++ RIM_simulation_iclamp.py
@@ -1,3 +1,8 @@
+# "Biophysical modeling of the whole-cell dynamics of C. elegans motor and interneurons families"
+# M. Nicoletti et al. PloS ONE, 19(3): e0298105.
+# https://doi.org/10.1371/journal.pone.0298105
+
+
 def RIM_simulation_iclamp(gRIM_scaled,s1,s2,ns):
     
  
