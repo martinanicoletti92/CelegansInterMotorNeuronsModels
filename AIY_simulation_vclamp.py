377ab5d8a7810f4721c76e68a91d56bed5c7fa47
martinanicoletti92 <34269874+martinanicoletti92@users.noreply.github.com>
2024-04-08 18:07:56 +0200

    Update AIY_simulation_vclamp.py

--- AIY_simulation_vclamp.py
+++ AIY_simulation_vclamp.py
@@ -1,3 +1,8 @@
+
+# "Biophysical modeling of the whole-cell dynamics of C. elegans motor and interneurons families"
+# M. Nicoletti et al. PloS ONE, 19(3): e0298105.
+# https://doi.org/10.1371/journal.pone.0298105
+
 def AIY_simulation_vc(gAIY_scaled,vstart,vstop,ns):
     
     # voltage clamp per Minimizzazioni con canali calcio fissati e ottimizzazione sul potassio
