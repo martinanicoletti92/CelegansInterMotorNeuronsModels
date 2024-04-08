6eed51ab9ed91cd8387caf471dbbad93927d67d3
martinanicoletti92 <34269874+martinanicoletti92@users.noreply.github.com>
2024-04-08 18:10:56 +0200

    Update VA5_simulation.py

--- VA5_simulation.py
+++ VA5_simulation.py
@@ -1,4 +1,8 @@
-# M. Nicoletti et al. 2023, "Biophysical modeling of the whole-cell dynamics of C. elegans motor and interneurons families"
+# "Biophysical modeling of the whole-cell dynamics of C. elegans motor and interneurons families"
+# M. Nicoletti et al. PloS ONE, 19(3): e0298105.
+# https://doi.org/10.1371/journal.pone.0298105
+
+
 # VA5 voltage and current clamp responses shown in Figure 7 panels A,D and G
 
 import os
