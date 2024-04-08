5b0db84cc685729f81810677521f637056fd85ac
martinanicoletti92 <34269874+martinanicoletti92@users.noreply.github.com>
2024-04-08 18:12:53 +0200

    Update VD5_simulation_iclamp.py

--- VD5_simulation_iclamp.py
+++ VD5_simulation_iclamp.py
@@ -1,3 +1,7 @@
+# "Biophysical modeling of the whole-cell dynamics of C. elegans motor and interneurons families"
+# M. Nicoletti et al. PloS ONE, 19(3): e0298105.
+# https://doi.org/10.1371/journal.pone.0298105
+
 def VD5_simulation_iclamp(gVD5_scaled,s1,s2,ns):
     
  
